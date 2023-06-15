bd5e12b00bb81eab61a25082b056c3b54e589b7c
WallMarty <136638237+WallMarty@users.noreply.github.com>
2023-06-15 03:08:30 +0300

    Tracker mail
    A simple mail tracker written in Python

---
+++ tracker_mail.py
@@ -0,0 +1,21 @@
+import requests
+
+def track_package(tracking_number):
+    url = f"https://api.shippingcarrier.com/track/{tracking_number}"
+    response = requests.get(url)
+    
+    if response.status_code == 200:
+        data = response.json()
+        status = data["status"]
+        location = data["location"]
+        estimated_delivery = data["estimated_delivery"]
+        
+        print("Package Status:", status)
+        print("Current Location:", location)
+        print("Estimated Delivery:", estimated_delivery)
+    else:
+        print("Failed to track package. Please check the tracking number.")
+
+# Пример использования
+tracking_number = "123456789"  # Замените на свой трекинг-номер
+track_package(tracking_number)
